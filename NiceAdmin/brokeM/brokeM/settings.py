508c36393a052150c246a7bf5c6b351a2f31e30e
Elsamu2213 <131383787+Elsamu2213@users.noreply.github.com>
2025-01-23 15:47:12 -0600

    Update settings.py

--- NiceAdmin/brokeM/brokeM/settings.py
+++ NiceAdmin/brokeM/brokeM/settings.py
@@ -19,7 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['broke-67fe.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
